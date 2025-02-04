9a1f59c93a71f29b146fa3b706cb15e55147c397
parvez <parvezz13913@gmail.com>
2025-02-04 18:32:20 +0600

    [DONE] Certificate section

--- src/app/components/certificate.tsx
+++ src/app/components/certificate.tsx
@@ -1,15 +1,21 @@
 "use client";
 import Image from "next/image";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import generatePDF from "react-to-pdf";
 
 function Certificate() {
   const [name, setName] = useState("");
-  const handleGenerate = () => {
+  const targetRef = useRef(null);
+
+  const handleClear = () => {
     setName("");
   };
+
   return (
-    <div className="p-4">
-      <h1 className="text-4xl text-center my-4">Generate your Certificate</h1>
+    <div className="p-4 bg-gradient-to-r from-violet-500 to-purple-500">
+      <h1 className="text-4xl text-center font-semibold my-4">
+        Generate Certificate
+      </h1>
       <div className="space-x-4 mx-auto w-fit m-4">
         <input
           type="text"
@@ -19,13 +25,28 @@ function Certificate() {
           className="flex-grow px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
         <button
-          onClick={handleGenerate}
-          className="w-full sm:w-auto px-6 py-2 bg-blue-600 text-white font-semibold rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+          onClick={handleClear}
+          className="px-6 py-2 bg-green-600 text-white font-semibold rounded-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500"
         >
           Clear
         </button>
+        {/* Download PDF Button */}
+
+        <button
+          onClick={() =>
+            generatePDF(targetRef, { filename: "certificate.pdf" })
+          }
+          className="px-6 py-2 bg-green-600 text-white font-semibold rounded-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500"
+        >
+          Download PDF
+        </button>
       </div>
-      <div className="flex items-center justify-center relative">
+
+      {/* Certificate Section to be downloaded */}
+      <div
+        ref={targetRef}
+        className="flex items-center justify-center relative p-4"
+      >
         <Image
           src="/certificate.png"
           alt="Certificate image"
@@ -33,7 +54,9 @@ function Certificate() {
           height={800}
           className="object-cover"
         />
-        <h1 className="absolute bottom-72 text-4xl">{name}</h1>
+        <h1 className="absolute bottom-80 text-4xl dancing-font font-bold">
+          {name}
+        </h1>
       </div>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
 import Certificate from "./components/certificate";
-import CTA from "./components/CTA";
-import Hero from "./components/hero";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-gray-100">
-      <Hero />
       <Certificate />
-      <CTA />
     </main>
   );
 }
